ef9e68f04ddd052d06f611e8ea239bb085d6f61d
Richard Bowden <rb@rdbg.co.uk>
2015-12-21 11:23:27 +0000

    initial

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+	"net/http"
+	"os"
+)
+
+const servePortEnv string = "RANNUM_PORT"
+const defaultServePort string = "8080"
+
+func genRndNumber(w rest.ResponseWriter, r *rest.Request) {
+	c := 10
+	b := make([]byte, c)
+	rand.Read(b)
+	rndNum := base64.URLEncoding.EncodeToString(b)
+
+	d := struct {
+		RndNum string
+	}{
+		rndNum,
+	}
+
+	w.WriteJson(d)
+	return
+}
+
+func main() {
+
+	servePort := os.Getenv(servePortEnv)
+
+	if servePort == "" {
+		servePort = defaultServePort
+	}
+
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+
+	router, err := rest.MakeRouter(
+		rest.Get("/rnd", genRndNumber),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api.SetApp(router)
+	bindPort := fmt.Sprintf(":%s", servePort)
+
+	log.Printf("server listening on %s", bindPort)
+
+	log.Fatal(http.ListenAndServe(bindPort, api.MakeHandler()))
+}
