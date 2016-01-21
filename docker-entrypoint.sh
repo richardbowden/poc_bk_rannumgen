5239b824e944d08dc8ed83e42f0a553c5e527b2d
asega olima <asega.olima@sainsburys.co.uk>
2016-01-21 12:08:12 +0000

    resolving the service config via spring-cloud-config-server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,8 +10,16 @@ header() {
 header "Starting up..."
 
 header "Resolving the configuration..."
-# TODO: Here we should resolve the configuration from the config service
-export RANNUM_PORT=9090
+
+# Resolving the configuration from the config service
+TMP=`tempfile`
+curl --silent http://configs:8888/frontend-${ENVIRONMENT}.properties > $TMP
+while read LINE; do
+    VAR=`echo $LINE | cut -d ':' -f 1`
+    VAL=`echo $LINE | cut -d ' ' -f 2`
+    export $VAR=$VAL
+done < $TMP
+rm -fr $TMP
 
 header "Running the application"
 #Start the application
